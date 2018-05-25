232bdf3e5c41deb3a07932cfc39b51f048360d3c
Lachlan Redman <lachlanredman10@gmail.com>
2018-05-25 12:59:41 +1000

    Initial Commit

---
+++ Storm Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Lachlan Redman on 25/5/18.
+//  Copyright © 2018 Lachlan Redman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
